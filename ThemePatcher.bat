77e3b5f49ead5247fac9ebad2d220464f255720b
Sayantan Paul <69689739+legendsayantan@users.noreply.github.com>
2022-03-20 19:20:16 +0530

    Add files via upload

--- ThemePatcher.bat
+++ ThemePatcher.bat
@@ -6,7 +6,7 @@ echo Take a free trial from theme store, then run this batch file to make them p
 echo.
 echo using && WHERE adb
 IF %ERRORLEVEL% NEQ 0 (
-ECHO ERROR ENCOUNTERED. Please put this bat file in adb directory of your system.
+ECHO ERROR ENCOUNTERED- Please put this bat file in ADB directory of your system.
 PAUSE
 exit
 )
@@ -16,15 +16,15 @@ set /p device=<tempfile.tmp
 set nodevice=no devices/emulators found
 set multidevice=more than one device/emulator
 IF "%device%" EQU "%multidevice%" (
-ECHO ERROR ENCOUNTERED. Multiple android devices are connected through adb.
+ECHO ERROR ENCOUNTERED- More than one android devices are connected through adb.
 PAUSE
 exit
 )
 IF "%device%" EQU "%nodevice%" (
-ECHO ERROR ENCOUNTERED. No android devices are connected through adb.
+ECHO ERROR ENCOUNTERED- No android devices are connected through adb.
 PAUSE
 exit
-) ELSE (echo Please wait ,connecting to android device...)
+) ELSE (echo Please wait , connecting to android device...)
 timeout /t 10 /nobreak
 adb shell am force-stop com.heytap.themestore
 adb shell am force-stop com.nearme.themestore
@@ -37,8 +37,7 @@ adb shell settings get system persist.sys.trial.live_wp >tempfile.tmp
 set /p livewp=<tempfile.tmp
 del tempfile.tmp
 IF %theme% NEQ 0 (
-echo Trial theme detected , patching theme...
-::Run these codes to patch themes manually
+echo Trial theme detected , converting to permanent...
 adb shell settings put system persist.sys.oppo.theme_uuid -1
 adb shell settings put system persist.sys.oplus.theme_uuid -1
 adb shell settings put system persist.sys.trial.theme 0
@@ -47,14 +46,12 @@ adb shell settings put secure persist.sys.oplus.theme_uuid -1
 adb shell settings put secure persist.sys.trial.theme 0
 ) ELSE (echo No Theme trials activated to patch.)
 IF %font% NEQ 0 (
-echo Trial font detected , patching font...
-::Run these codes to patch fonts manually
+echo Trial font detected , converting to permanent...
 adb shell settings put system persist.sys.trial.font 0
 adb shell settings put secure persist.sys.trial.font 0
 ) ELSE (echo No Font trials activated to patch.)
 IF %livewp% NEQ 0 (
-echo Trial live wallpaper detected , patching live wallpaper...
-::Run these codes to patch live wallpapers manually
+echo Trial live wallpaper detected , converting to permanent...
 adb shell settings put system persist.sys.oplus.live_wp_uuid default_live_wp_package_name
 adb shell settings put system persist.sys.oppo.live_wp_uuid -1
 adb shell settings put system persist.sys.trial.live_wp 0
